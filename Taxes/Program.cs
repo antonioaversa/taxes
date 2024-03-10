63d5ea1148b665eb669e3b7cf9706ab1f39923a5
Antonio Aversa <differenziale@gmail.com>
2024-03-10 14:46:15 +0100

    Print ticker states metrics via reflection on Metric-decorated properties

--- Taxes/Program.cs
+++ Taxes/Program.cs
@@ -23,22 +23,5 @@ static void ProcessEvents(IList<Event> events)
         tickerStates.Add(TickerProcessing.ProcessTicker(ticker, tickerEvents));
     }
 
-    var totalPlusValueCumpBase = tickerStates.Sum(ts => ts.PlusValueCumpBase);
-    var totalPlusValuePepsBase = tickerStates.Sum(ts => ts.PlusValuePepsBase);
-    var totalPlusValueCryptoBase = tickerStates.Sum(ts => ts.PlusValueCryptoBase);
-    var totalMinusValueCumpBase = tickerStates.Sum(ts => ts.MinusValueCumpBase);
-    var totalMinusValuePepsBase = tickerStates.Sum(ts => ts.MinusValuePepsBase);
-    var totalMinusValueCryptoBase = tickerStates.Sum(ts => ts.MinusValueCryptoBase);
-    var totalNetDividendsBase = tickerStates.Sum(ts => ts.NetDividendsBase);
-    var totalWhtDividendsBase = tickerStates.Sum(ts => ts.WhtDividendsBase);
-    var totalGrossDividendsBase = tickerStates.Sum(ts => ts.GrossDividendsBase);
-    Console.WriteLine($"Total Plus Value CUMP ({BaseCurrency}) = {totalPlusValueCumpBase.R()}");
-    Console.WriteLine($"Total Plus Value PEPS ({BaseCurrency}) = {totalPlusValuePepsBase.R()}");
-    Console.WriteLine($"Total Plus Value CRYPTO ({BaseCurrency}) = {totalPlusValueCryptoBase.R()}");
-    Console.WriteLine($"Total Minus Value CUMP ({BaseCurrency}) = {totalMinusValueCumpBase.R()}");
-    Console.WriteLine($"Total Minus Value PEPS ({BaseCurrency}) = {totalMinusValuePepsBase.R()}");
-    Console.WriteLine($"Total Minus Value CRYPTO ({BaseCurrency}) = {totalMinusValueCryptoBase.R()}");
-    Console.WriteLine($"Total Net Dividends ({BaseCurrency}) = {totalNetDividendsBase.R()}");
-    Console.WriteLine($"Total WHT Dividends ({BaseCurrency}) = {totalWhtDividendsBase.R()}");
-    Console.WriteLine($"Total Gross Dividends ({BaseCurrency}) = {totalGrossDividendsBase.R()}");
+    tickerStates.PrintAggregatedMetrics();
 }
--- Taxes/TickerState.cs
+++ Taxes/TickerState.cs
@@ -1,15 +1,32 @@
 ﻿namespace Taxes;
 
+using System.Reflection;
 using static Basics;
 
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+class MetricAttribute(string description) : Attribute
+{
+    public string Description { get; } = description;
+}
+
 record TickerState(
-    string Ticker, string Isin,
-    decimal PlusValueCumpBase = 0m, decimal PlusValuePepsBase = 0m, decimal PlusValueCryptoBase = 0m,
-    decimal MinusValueCumpBase = 0m, decimal MinusValuePepsBase = 0m, decimal MinusValueCryptoBase = 0m,
-    decimal TotalQuantity = 0m, decimal TotalAmountBase = 0m,
-    decimal NetDividendsBase = 0m, decimal WhtDividendsBase = 0m, decimal GrossDividendsBase = 0m,
-    int PepsCurrentIndex = 0, decimal PepsCurrentIndexBoughtQuantity = 0m,
-    decimal PortfolioAcquisitionValueBase = 0m, decimal CryptoFractionOfInitialCapital = 0m)
+    string Ticker, 
+    string Isin,
+    [Metric("Total Plus Value CUMP")] decimal PlusValueCumpBase = 0m,
+    [Metric("Total Plus Value PEPS")] decimal PlusValuePepsBase = 0m,
+    [Metric("Total Plus Value CRYPTO")] decimal PlusValueCryptoBase = 0m,
+    [Metric("Total Minus Value CUMP")] decimal MinusValueCumpBase = 0m,
+    [Metric("Total Minus Value PEPS")] decimal MinusValuePepsBase = 0m,
+    [Metric("Total Minus Value CRYPTO")] decimal MinusValueCryptoBase = 0m,
+    decimal TotalQuantity = 0m,
+    decimal TotalAmountBase = 0m,
+    [Metric("Total Net Dividends")] decimal NetDividendsBase = 0m,
+    [Metric("Total WHT Dividends")] decimal WhtDividendsBase = 0m,
+    [Metric("Total Gross Dividends")] decimal GrossDividendsBase = 0m,
+    int PepsCurrentIndex = 0, 
+    decimal PepsCurrentIndexBoughtQuantity = 0m,
+    decimal PortfolioAcquisitionValueBase = 0m, 
+    decimal CryptoFractionOfInitialCapital = 0m)
 {
     public override string ToString() =>
         $"{TotalQuantity.R()} shares => {TotalAmountBase.R()} {BaseCurrency}, " +
@@ -19,3 +36,18 @@ record TickerState(
 }
 
 delegate TickerState TickerAction(Event tickerEvent, IList<Event> tickerEvents, int eventIndex, TickerState tickerState);
+
+internal static class TickerStateListExtensions
+{
+    internal static void PrintAggregatedMetrics(this IEnumerable<TickerState> tickerStates)
+    {
+         var metricStrings = 
+             from property in typeof(TickerState).GetProperties()
+             let metricAttribute = property.GetCustomAttribute<MetricAttribute>()
+             where metricAttribute is not null
+             let metricSum = tickerStates.Sum(ts => (decimal)property.GetValue(ts))
+             select $"{metricAttribute.Description} ({BaseCurrency}) = {metricSum.R()}";
+
+        metricStrings.ToList().ForEach(Console.WriteLine);
+    }
+}
