c170c5f31ecb0913d6f57eb8ea69c6f930cfea4e
Antonio Aversa <differenziale@gmail.com>
2024-03-10 23:34:44 +0100

    Move Reports to project level and include in project

--- Taxes.Tests/TickerProcessingTest.cs
+++ Taxes.Tests/TickerProcessingTest.cs
@@ -1,11 +1,28 @@
 namespace Taxes.Test;
 
+using static TickerProcessing;
+using static EventType;
+
 [TestClass]
 public class TickerProcessingTest
 {
+    private const string Ticker = "AAPL";
+    private static readonly DateTime T0 = new DateTime(2022, 01, 01, 00, 00, 00);
+    private static readonly TimeSpan D = TimeSpan.FromDays(1);
+
     [TestMethod]
-    public void ProcessTicker()
-    {
-        // TODO: TickerProcessing.ProcessTicker()
-    }
+    public void ProcessTicker_BuyLimit() =>
+        ProcessTicker(Ticker, [new Event(T0, BuyLimit, Ticker, 3, 100, 303, 3, "EUR", 1, 0)]);
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+    public void ProcessTicker_SellWithoutBuying() => 
+        ProcessTicker(Ticker, [new Event(T0, SellLimit, Ticker, 3, 100, 303, 3, "EUR", 1, 0)]);
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+    public void ProcessTicker_SellMoreThanBuying() =>
+         ProcessTicker(Ticker, [
+             new Event(T0, BuyLimit, Ticker, 3, 100, 303, 3, "EUR", 1, 0),
+             new Event(T0, SellLimit, Ticker, 4, 100, 404, 4, "EUR", 1, 0)]);
 }
