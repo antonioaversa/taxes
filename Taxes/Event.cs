a429eb4309b6b8dedf3ed9b76f31bccf8829b99b
Antonio Aversa <differenziale@gmail.com>
2024-04-07 22:48:27 +0200

    Ticker Event: improve documentation

--- Taxes/Event.cs
+++ Taxes/Event.cs
@@ -38,19 +38,40 @@ record Event(
     decimal? Quantity,
 
     decimal? PricePerShareLocal,
+
+    /// <summary>
+    /// In the case of CustodyFee, it's the total amount of the fee.
+    /// </summary>
     decimal? TotalAmountLocal,
 
     /// <summary>
     /// Mandatory for Buy* and Sell*. Null otherwise.
     /// - It cannot be calculated for Dividend, as there is not enough information in the input file.
     /// - It doesn't make sense for StockSplit, as it's not a transaction, so fees are not applied.
+    /// - It's not defined for CustodyFee: the fee is conventionally defined as a TotalAmountLocal.
     /// Automatically calculated:
     /// - for Buy events as TotalAmountLocal - Quantity * PricePerShareLocal
     /// - for Sell events as Quantity * PricePerShareLocal - TotalAmountLocal
     /// </summary>
     decimal? FeesLocal,
+
+    /// <summary>
+    /// The currency in which the event has happened. 
+    /// If the event is about a transaction, this is the currency in which the transaction took place.
+    /// The currency is mandatory for all types of events, and relevant for all of them except for Reset.
+    /// </summary>
     string Currency,
+
+    /// <summary>
+    /// The exchange rate between the currency of the event and the base currency.
+    /// This exchange rate is defined by the broker providing the input file with all the events.
+    /// It is not the exchange rate as defined by the European Central Bank.
+    /// So it's not the use to be used for tax purposes, unless no ECB rate is available for the date of the event.
+    /// That can happen, for example, for events happening on weekends or holidays, when the ECB doesn't provide rates.
+    /// An example of such scenario is for crypto, that are traded 24/7.
+    /// </summary>
     decimal FXRate,
+
     decimal PortfolioCurrentValueBase)
 {
     public bool IsBuy => Type is EventType.BuyMarket or EventType.BuyLimit;
