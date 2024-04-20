d639f01368052b8cbc027b796464453516656a12
Antonio Aversa <differenziale@gmail.com>
2024-04-20 13:22:04 +0200

    Improve documentation

--- Taxes/DecimalExtensions.cs
+++ Taxes/DecimalExtensions.cs
@@ -2,5 +2,8 @@
 
 public static class DecimalExtensions
 {
+    /// <summary>
+    /// Shortcut to round a decimal via the Rounding method defined in the provided Basics 
+    /// </summary>
     public static decimal R(this decimal value, Basics basics) => basics.Rounding(value);
 }
