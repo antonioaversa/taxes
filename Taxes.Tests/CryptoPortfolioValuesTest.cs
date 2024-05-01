50048b3c59b5cbe27122aa1da337ca40938ed48f
Antonio Aversa <differenziale@gmail.com>
2024-05-01 22:57:47 +0200

    CryptoPortfolioValues: extend coverage

--- Taxes.Tests/CryptoPortfolioValuesTest.cs
+++ Taxes.Tests/CryptoPortfolioValuesTest.cs
@@ -1,4 +1,5 @@
 ﻿using Taxes.Test;
+using static Taxes.Test.AssertExtensions;
 
 namespace Taxes.Tests;
 
@@ -7,11 +8,12 @@ public class CryptoPortfolioValuesTest
 {
     private const string CHF = "CHF";
     private const string USD = "USD";
+    private const string GBP = "GBP";
 
     private Basics Basics { get; } = new();
 
     [TestMethod]
-    public void ReadsValuesCorrectly()
+    public void Parse_ReadsTheRightCurrency()
     {
         var fxRates = new FxRates(new() 
             {
@@ -29,16 +31,30 @@ public class CryptoPortfolioValuesTest
                     },
             });
 
-        var cryptoPortfolioValues = new CryptoPortfolioValues(Basics, fxRates, USD, new StringReader("""
+        static StringReader portfolioValuesReader() => new StringReader("""
             Date,PortfolioValue
             2021-01-01,0
             2021-01-02,1234
             2021-01-03,5.67
-            """));
+            """);
 
-        AssertEq(0m, cryptoPortfolioValues[(2021, 1, 1).ToUtc()]);
-        AssertEq(1234m / 1.1m, cryptoPortfolioValues[(2021, 1, 2).ToUtc()]);
-        AssertEq(5.67m / 1.2m, cryptoPortfolioValues[(2021, 1, 3).ToUtc()]);
+        var cryptoPortfolioValuesUSD = new CryptoPortfolioValues(Basics, fxRates, USD, portfolioValuesReader());
+        AssertEq(0m / 1.0m, cryptoPortfolioValuesUSD[(2021, 1, 1).ToUtc()]);
+        AssertEq(1234m / 1.1m, cryptoPortfolioValuesUSD[(2021, 1, 2).ToUtc()]);
+        AssertEq(5.67m / 1.2m, cryptoPortfolioValuesUSD[(2021, 1, 3).ToUtc()]);
+        AssertEq(-1m, cryptoPortfolioValuesUSD[(2021, 1, 4).ToUtc()]); // No portfolio value for this date
+
+        var cryptoPortfolioValuesCHF = new CryptoPortfolioValues(Basics, fxRates, CHF, portfolioValuesReader());
+        AssertEq(0m / 0.9m, cryptoPortfolioValuesCHF[(2021, 1, 1).ToUtc()]);
+        AssertEq(1234m / 0.8m, cryptoPortfolioValuesCHF[(2021, 1, 2).ToUtc()]);
+        AssertEq(5.67m / 0.7m, cryptoPortfolioValuesCHF[(2021, 1, 3).ToUtc()]);
+        AssertEq(-1m, cryptoPortfolioValuesCHF[(2021, 1, 4).ToUtc()]); // No portfolio value for this date
+
+        // No FX Rates for GBP
+        var cryptoPortfolioValuesGBP = new CryptoPortfolioValues(Basics, fxRates, GBP, portfolioValuesReader());
+        ThrowsAny<InvalidDataException>(() => _ = cryptoPortfolioValuesGBP[(2021, 1, 1).ToUtc()]);
+        ThrowsAny<InvalidDataException>(() => _ = cryptoPortfolioValuesGBP[(2021, 1, 2).ToUtc()]);
+        ThrowsAny<InvalidDataException>(() => _ = cryptoPortfolioValuesGBP[(2021, 1, 3).ToUtc()]);
     }
 
     [AssertionMethod]
