6654d22c9efca7a85afd6b6c6c48c36209d8f944
Antonio Aversa <differenziale@gmail.com>
2025-05-11 23:55:55 +0200

    Program: factorize code in common between stocks and crypto

--- Taxes/Program.cs
+++ Taxes/Program.cs
@@ -13,79 +13,81 @@ var cryptoPortfolioValues = new CryptoPortfolioValues(basics, fxRates, cryptoPor
 
 await outWriters.Default.WriteLineAsync("# STOCKS");
 var stockEventsReader = new StockEventsReader(basics);
-var stockEvents = basics.StockEventsFiles
-    .SelectMany(eventsFiles => Directory
-        .GetFiles(basics.ReportsDirectoryPath, eventsFiles.FilePattern)
-        .EnsureNonEmpty()
-        .Select(path => new EventsFileAndBroker(path, eventsFiles.Broker)))
-    .OrderBy(eventsFileAndBroker => eventsFileAndBroker.FilePath)
-    .PrintEachElement(outWriters.Default, eventsFileAndBroker => $"Parsing File {eventsFileAndBroker.FilePath}...")
+basics.StockEventsFiles
+    .FindEventsFiles(basics, outWriters)
     .SelectMany(eventsFileAndBroker => stockEventsReader
         .Parse(eventsFileAndBroker.FilePath, fxRates, eventsFileAndBroker.Broker, outWriters.Default))
     .PrintEachElement(outWriters.Default, @event => $"Parsing Event {@event}...")
-    .ToList();
-ProcessEvents(stockEvents, basics, cryptoPortfolioValues, outWriters);
+    .ToList()
+    .ProcessEvents(basics, cryptoPortfolioValues, outWriters);
 
 await outWriters.Default.WriteLineAsync("# CRYPTO");
 var cryptoEventsReader = new CryptoEventsReader(basics);
-var cryptoEvents = basics.CryptoEventsFiles
-    .SelectMany(eventsFiles => Directory
-        .GetFiles(basics.ReportsDirectoryPath, eventsFiles.FilePattern)
-        .EnsureNonEmpty()
-        .Select(path => new EventsFileAndBroker(path, eventsFiles.Broker)))
-    .OrderBy(eventsFileAndBroker => eventsFileAndBroker.FilePath)
-    .PrintEachElement(outWriters.Default, filePath => $"Parsing File {filePath}...")
+basics.CryptoEventsFiles
+    .FindEventsFiles(basics, outWriters)
     .SelectMany(eventsFileAndBroker => cryptoEventsReader
         .ParseFile(eventsFileAndBroker.FilePath, fxRates, eventsFileAndBroker.Broker, outWriters.Default))
     .PrintEachElement(outWriters.Default, @event => $"Parsing Event {@event}...")
-    .ToList();
-ProcessEvents(cryptoEvents, basics, cryptoPortfolioValues, outWriters);
+    .ToList()
+    .ProcessEvents(basics, cryptoPortfolioValues, outWriters);
 
-return;
-
-static void ProcessEvents(IList<Event> events, Basics basics, CryptoPortfolioValues cryptoPortfolioValues, OutWriters outWriters)
+namespace Taxes
 {
-    var tickerProcessing = new TickerProcessing(basics, cryptoPortfolioValues);
-
-    // Taken into account in each ticker
-    var nonTickerRelatedEvents = (
-        from e in events
-        where string.IsNullOrWhiteSpace(e.Ticker)
-        select e)
-        .ToList();
-    var eventsByTicker = (
-        from e in events
-        group e by e.Ticker into g
-        orderby g.Key
-        select (ticker: g.Key, tickerEvents: g.Concat(nonTickerRelatedEvents).OrderBy(e1 => e1.Date).ToArray()))
-        .ToList();
-
-    var tickerStates = (
-        from e in eventsByTicker
-        select tickerProcessing.ProcessTicker(e.ticker, e.tickerEvents, outWriters))
-        .ToList();
-
-    tickerStates.PrintAggregatedMetrics(outWriters.Default, basics);
-
-    var anyCryptoEvent = events.Any(e => 
-        e.Ticker == CryptoEventsReader.CryptoTicker);
-    var anyNonCryptoEvent = events.Any(e => 
-        !string.IsNullOrEmpty(e.Ticker) && e.Ticker != CryptoEventsReader.CryptoTicker);
-    
-    switch (anyCryptoEvent, anyNonCryptoEvent)
+    static class Extensions
     {
-        case (true, true):
-            throw new InvalidOperationException(
-                $"Cannot process crypto and non-crypto events at the same time. Please split them into different {nameof(ProcessEvents)} executions.");
-        case (true, false):
-            outWriters.Default.Write(outWriters.Form2086Writer.ToString());
-            break;
-        case (false, true):
-            outWriters.Default.Write(outWriters.Form2047Writer.ToString());
-            break;
-        case (false, false):
-            throw new InvalidOperationException(
-                "Neither crypto nor non-crypto events found. Please check your input files.");
+        public static IEnumerable<EventsFileAndBroker> FindEventsFiles(this IEnumerable<Basics.EventsFiles> files, Basics basics, OutWriters outWriters) =>
+            files.SelectMany(eventsFiles => Directory
+                    .GetFiles(basics.ReportsDirectoryPath, eventsFiles.FilePattern)
+                    .EnsureNonEmpty()
+                    .Select(path => new EventsFileAndBroker(path, eventsFiles.Broker)))
+                .OrderBy(eventsFileAndBroker => eventsFileAndBroker.FilePath)
+                .PrintEachElement(outWriters.Default, filePath => $"Parsing File {filePath}...");
+
+        public static void ProcessEvents(this IList<Event> events, Basics basics, CryptoPortfolioValues cryptoPortfolioValues, OutWriters outWriters)
+        {
+            var tickerProcessing = new TickerProcessing(basics, cryptoPortfolioValues);
+
+            // Taken into account in each ticker
+            var nonTickerRelatedEvents = (
+                    from e in events
+                    where string.IsNullOrWhiteSpace(e.Ticker)
+                    select e)
+                .ToList();
+            var eventsByTicker = (
+                    from e in events
+                    group e by e.Ticker
+                    into g
+                    orderby g.Key
+                    select (ticker: g.Key, tickerEvents: g.Concat(nonTickerRelatedEvents).OrderBy(e1 => e1.Date).ToArray()))
+                .ToList();
+
+            var tickerStates = (
+                    from e in eventsByTicker
+                    select tickerProcessing.ProcessTicker(e.ticker, e.tickerEvents, outWriters))
+                .ToList();
+
+            tickerStates.PrintAggregatedMetrics(outWriters.Default, basics);
+
+            var anyCryptoEvent = events.Any(e =>
+                e.Ticker == CryptoEventsReader.CryptoTicker);
+            var anyNonCryptoEvent = events.Any(e =>
+                !string.IsNullOrEmpty(e.Ticker) && e.Ticker != CryptoEventsReader.CryptoTicker);
+
+            switch (anyCryptoEvent, anyNonCryptoEvent)
+            {
+                case (true, true):
+                    throw new InvalidOperationException(
+                        $"Cannot process crypto and non-crypto events at the same time. Please split them into different {nameof(ProcessEvents)} executions.");
+                case (true, false):
+                    outWriters.Default.Write(outWriters.Form2086Writer.ToString());
+                    break;
+                case (false, true):
+                    outWriters.Default.Write(outWriters.Form2047Writer.ToString());
+                    break;
+                case (false, false):
+                    throw new InvalidOperationException(
+                        "Neither crypto nor non-crypto events found. Please check your input files.");
+            }
+        }
     }
 }
-
